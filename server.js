4b545fb8a23152f75ebf4f3c36e49cff755af887
ngbricen <bricen@gmail.com>
2017-08-08 09:38:49 -0400

    Including all questions as check was working on only 2 questions

--- server.js
+++ server.js
@@ -88,6 +88,14 @@ function compareFriends(newFriend){
       //Compare values between friends and new survey (new friend)
       sumDifference = Math.abs(parseInt(friends[key]["q1"]) - parseInt(newFriend["q1"]))
                     + Math.abs(parseInt(friends[key]["q2"]) - parseInt(newFriend["q2"]))
+                    + Math.abs(parseInt(friends[key]["q3"]) - parseInt(newFriend["q3"]))
+                    + Math.abs(parseInt(friends[key]["q4"]) - parseInt(newFriend["q4"]))
+                    + Math.abs(parseInt(friends[key]["q5"]) - parseInt(newFriend["q5"]))
+                    + Math.abs(parseInt(friends[key]["q6"]) - parseInt(newFriend["q6"]))
+                    + Math.abs(parseInt(friends[key]["q7"]) - parseInt(newFriend["q7"]))
+                    + Math.abs(parseInt(friends[key]["q8"]) - parseInt(newFriend["q8"]))
+                    + Math.abs(parseInt(friends[key]["q9"]) - parseInt(newFriend["q9"]))
+                    + Math.abs(parseInt(friends[key]["q10"]) - parseInt(newFriend["q10"]))
 
       //If closer match, need to capture user and picture
       if (sumDifference <= minDifference){
